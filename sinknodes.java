28598d32e6f96621df4c27ade543d71e9e969e31
karthikesh <karthikesh04.m@gmail.com>
2026-02-13 11:11:59 +0530

    first commit

---
+++ sinknodes.java
@@ -0,0 +1,20 @@
+public class sinknodes {
+    public static void main(String[] args) {
+        int zero = 0;
+        int arr[][] = { { 0, 1, 1, 0, 1 }, { 0, 0, 0, 1, 0 }, { 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+        for (int i = 0; i < arr.length; i++) {
+            zero = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] == 0) {
+                    zero++;
+                }
+
+            }
+            if (zero == arr.length) {
+                System.out.println("Sink Nodes " + (char) (65 + i)); //explicit type casting 
+            }
+            
+            
+        }
+    }
+}
