5f522af5830c98207303801803dc56b9dc82ea4c
karthikesh <karthikesh04.m@gmail.com>
2026-02-28 17:36:59 +0530

    Write name to file

--- WriteNameToFile.java
+++ WriteNameToFile.java
@@ -10,16 +10,18 @@ public class WriteNameToFile
 			FileWriter obj = new FileWriter("Student.txt", true);
 			Scanner in = new Scanner(System.in);
 			int n = in.nextInt();
-			for(int i =0;i<=n;i++){
-			System.out.println("Enter a Word");		
-			String st = in.next();
-			obj.write(st +"\n");
-			System.out.println("Name Written Successfully");
+			for (int i = 0; i <= n; i++) 
+			{
+					System.out.println("Enter a Word");		
+					String st = in.next();
+					obj.write(st +"\n");
+					System.out.println("Name Written Successfully");
 			}
 			obj.close();	
 		
 		}
-		catch(IOException e){
+		catch (IOException e) 
+		{
 			System.out.println("An error occurred");
 			e.printStackTrace();
 		}
